c019bab113909505de387e97e4651689141224b5
franrbb <fran89rbb@hotmail.com>
2023-10-13 21:43:35 +0200

    commit

--- src/main/java/com/example/demo/entity/Prueba.java
+++ src/main/java/com/example/demo/entity/Prueba.java
@@ -1,10 +1,14 @@
 package com.example.demo.entity;
 
 public class Prueba {
-	
+
 	private String main1;
-	
+
 	private String main2;
+
+	private String main3;
+
+	private String main4;
 
 	public String getMain1() {
 		return main1;
@@ -21,5 +25,21 @@ public class Prueba {
 	public void setMain2(String main2) {
 		this.main2 = main2;
 	}
-	
+
+	public String getMain3() {
+		return main3;
+	}
+
+	public void setMain3(String main3) {
+		this.main3 = main3;
+	}
+
+	public String getMain4() {
+		return main4;
+	}
+
+	public void setMain4(String main4) {
+		this.main4 = main4;
+	}
+
 }
