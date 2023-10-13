000019490f6db35306c865541412bf772d7b54d3
franrbb <fran89rbb@hotmail.com>
2023-10-13 21:37:42 +0200

    commit

---
+++ src/main/java/com/example/demo/entity/Prueba.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+public class Prueba {
+	
+	private String main1;
+	
+	private String main2;
+
+	public String getMain1() {
+		return main1;
+	}
+
+	public void setMain1(String main1) {
+		this.main1 = main1;
+	}
+
+	public String getMain2() {
+		return main2;
+	}
+
+	public void setMain2(String main2) {
+		this.main2 = main2;
+	}
+	
+}
