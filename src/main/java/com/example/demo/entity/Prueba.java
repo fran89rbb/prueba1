6de57ccfbb950fbd0f09f9d4efb1d8753ad18633
franrbb <fran89rbb@hotmail.com>
2023-10-13 21:41:35 +0200

    commiy

--- src/main/java/com/example/demo/entity/Prueba.java
+++ src/main/java/com/example/demo/entity/Prueba.java
@@ -1,10 +1,14 @@
 package com.example.demo.entity;
 
 public class Prueba {
-	
+
 	private String main1;
-	
+
 	private String main2;
+
+	private String rama1;
+
+	private String rama2;
 
 	public String getMain1() {
 		return main1;
@@ -21,5 +25,21 @@ public class Prueba {
 	public void setMain2(String main2) {
 		this.main2 = main2;
 	}
-	
+
+	public String getRama1() {
+		return rama1;
+	}
+
+	public void setRama1(String rama1) {
+		this.rama1 = rama1;
+	}
+
+	public String getRama2() {
+		return rama2;
+	}
+
+	public void setRama2(String rama2) {
+		this.rama2 = rama2;
+	}
+
 }
